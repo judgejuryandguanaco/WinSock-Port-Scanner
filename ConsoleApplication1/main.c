f1a94c028bffc5b5263eefc5b14a79cab5b8cf25
mitch <mitchell.smith714@gmail.com>
2016-12-17 21:16:22 +0000

    Set number of simultaneous threads to 60 and now works

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -28,33 +28,43 @@ void scanport(char *hostname, char *port);
 DWORD WINAPI scanportthread(LPVOID lpParam);
 
 
-struct scp_prm {
+struct _args {
 	char hostname[1000];
 	char port[100];
-	DWORD n;
-} _scp_parm;
+	int n;
+} args;
 
-#define MAX_THREADS 100
+/** Don't use any more threads than this or it falls apart... */
+#define MAX_THREADS 60
 
 static HANDLE threads[MAX_THREADS];
 
 
 int main(int argc, char **argv)
 {
+	/** Function return value */
 	int ret = 0;
+	/** Value returned from other functions; for error checking */
 	int recv;
+	/** */
 	WSADATA wsa;
-	struct addrinfo *dest = NULL;
-	struct addrinfo hint;
-	SOCKET sock = INVALID_SOCKET;
-	char address[] = "www.google.com";
+	/** Hostname to scan */
+	char hostname[] = "www.google.com";
+	/** First port in range to scan */
 	int port1 = 1;
-	int port2 = 200;
-	DWORD threadids[MAX_THREADS];
-	struct scp_prm params[MAX_THREADS];
-	int numthreads = 0;
+	/** Last port in range to scan */
+	int port2 = 5000;
+	/** Array containing thread IDs */
+	DWORD thd_ids[MAX_THREADS];
+	/** Array of structs containing thread function arguments */
+	struct _args thd_args[MAX_THREADS];
+	/** Tracks number of threads active */
+	int n_thds = 0;
+	/** Number of do-while loops program has been through */
 	int cycles = 0;
+	/** Tracks whether should exit do-while loop */
 	int end = 0;
+	HANDLE handle;
 
 	/* Initialise WinSock2 */
 	recv = WSAStartup(MAKEWORD(2, 2), &wsa);
@@ -62,60 +72,62 @@ int main(int argc, char **argv)
 		print_wsastartup_error(recv);
 		goto main_end;
 	}
-	
-	/* Create socket to addr:port, try to connect */
-	//printf("Scanning %s between ports %d and %d\n", address, port1, port2);
-	
+
+	/** Scan MAX_THREADS ports at a time, keep going through this 
+	  * loop until we've covered the whole range 
+	  */
 	do {
+		/** The first port in the current range to scan */
 		int range1 = port1 + (MAX_THREADS * cycles++);
+		/** Second port in current range to scan */
 		int range2 = range1 + MAX_THREADS - 1;
+		/* If range2 larger than largest port num to scan, then set it
+		 * to largest port num
+		 */
 		if (range2 >= port2) {
 			range2 = port2;
 			end = 1;
 		}
-		printf("cycle %d: %d to %d\n", cycles, range1, range2);
+		handle = GetStdHandle(STD_OUTPUT_HANDLE);
+		if (handle != INVALID_HANDLE_VALUE) {
+			printf("cycle %d: %d to %d\n", cycles, range1, range2);
+		}
 
-		for (int i = range1, j = 0; i <= range2; i++, j++) {
+		/** Initialise thd_args, create thread, and keep track of
+		  *	no of threads created
+		  * i: Current port number
+		  * n: Position in thread array
+		  */
+		for (int i = range1, n = 0; i <= range2; i++, n++) {
+			/** Current port number as a string */
 			char port[10];
+			/** Convert i to string */
 			_itoa_s(i, &port, 10, 10);
-			strcpy_s(params[j].hostname, sizeof(params[j].hostname), address);
-			strcpy_s(params[j].port, sizeof(params[j].port), port);
-			params[j].n = j;
+			strcpy_s(thd_args[n].hostname, sizeof(thd_args[n].hostname), hostname);
+			strcpy_s(thd_args[n].port, sizeof(thd_args[n].port), port);
+			thd_args[n].n = n;
 
 			/* Create a new thread */
-			threads[j] = CreateThread(NULL, 0, scanportthread,
-										&params[j], 0,
-										&threadids[j]);
-			if (threads[j] == NULL) {
-				printf("threads[%d] == NULL\n", j);
+			threads[n] = CreateThread(NULL, 0, scanportthread,
+										&thd_args[n], 0,
+										&thd_ids[n]);
+			if (threads[n] == NULL) {
+				printf("threads[%d] == NULL\n", n);
 				ExitProcess(3);
 			}
-			numthreads = j + 1;
-			//printf("Created thread no. %d\n", j);
-			//scanport(address, port);
-		}
-		printf("numthreads = %d\n", numthreads);
-		recv = WaitForMultipleObjects((DWORD)numthreads, 
-										(HANDLE)threads    , 
-										(BOOL)TRUE, 
-										(DWORD)5000);
-		switch (recv) {
-		case WAIT_ABANDONED_0:
-			printf("WAIT_ABANDONED\n");
-			break;
-		case WAIT_TIMEOUT:
-			printf("WAIT_TIMEOUT\n");
-			break;
-		case WAIT_FAILED:
-			printf("WAIT_FAILED: %d\n", GetLastError());
-			break;
-		default:
-			printf("ARSE\n");
-			break;
+			/* Number of threads is one larger than position in array */
+			n_thds = n + 1;
 		}
 
-		printf("Closing threads\n");
-		for (int i = 0; i < numthreads; i++) {
+		/* Wait for all of the current threads to finish */
+		recv = WaitForMultipleObjects((DWORD)n_thds, 
+										(HANDLE)threads, 
+										(BOOL)TRUE, 
+										(DWORD)INFINITE);
+
+
+		/* Close all of the threads */
+		for (int i = 0; i < n_thds; i++) {
 			CloseHandle(threads[i]);
 		}
 	} while (end == 0);
@@ -132,103 +144,71 @@ DWORD WINAPI scanportthread(LPVOID lpParam)
 	HANDLE handle;
 	int ret = 0;
 	int recv;
+	int n;
 	char* hostname = NULL;
 	char* port = NULL;
-	TCHAR msg[100]; // w is TCHAR?
-	size_t msg_buf_siz = 100;
-	size_t msgsiz;
-	DWORD chars; // wtf is this for?
 	struct addrinfo *dest = NULL;
 	struct addrinfo hint;
 	SOCKET sock = INVALID_SOCKET;
 
+	/* Initialise hint */
 	ZeroMemory(&hint, sizeof(hint));
 	hint.ai_family = AF_INET;
 	hint.ai_socktype = SOCK_STREAM;
 	hint.ai_protocol = IPPROTO_TCP;
 
-	hostname = ((struct scp_prm *)lpParam)->hostname;
-	port = ((struct scp_prm *)lpParam)->port;
+	hostname = ((struct _args *)lpParam)->hostname;
+	port = ((struct _args *)lpParam)->port;
+	n = ((struct _args *)lpParam)->n;
 
 	recv = getaddrinfo(hostname, port, &hint, &dest);
 	if (recv == SOCKET_ERROR) {
 		print_getaddrinfo_error(recv);
-		goto thread_error;
+		goto thread_end;
 	}
 
 	sock = socket(dest->ai_family, dest->ai_socktype, dest->ai_protocol);
 	if (sock == INVALID_SOCKET) {
-		printf("%s INVALID SOCKET\n", port);
-		goto thread_error;
+		handle = GetStdHandle(STD_OUTPUT_HANDLE);
+		if (handle != INVALID_HANDLE_VALUE) {
+			printf("%s INVALID SOCKET\n", port);
+		}
+		goto thread_end;
 	}
 	recv = connect(sock, dest->ai_addr, (int)dest->ai_addrlen);
 	if (recv == SOCKET_ERROR) {
-		printf("%s filtered\n", port);
-		goto thread_error;
+		handle = GetStdHandle(STD_OUTPUT_HANDLE);
+		if (handle != INVALID_HANDLE_VALUE) {
+			//printf("%s filtered\n", port);
+		}
+		goto thread_end;
 	}
 
 	handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	if (handle != INVALID_HANDLE_VALUE) {
-		printf("%s open", port);
+		printf("%s open\n", port);
 	}
-thread_error:
-	SetEvent(threads[((struct scp_prm *)lpParam)->n]);
+thread_end:
+	//SetEvent(threads[n]);
 	closesocket(sock);
 	return ret;
 }
 
-void scanport(char *hostname, char *port)
+void print_waitformultipleobjects_error(int error)
 {
-	int recv;
-	struct addrinfo *dest = NULL;
-	struct addrinfo hint;
-	SOCKET sock = INVALID_SOCKET;
-	char ip[100];
-	DWORD sizeofip = (DWORD)sizeof(ip);
-	LPWSAPROTOCOL_INFO *lpProtocolInfo = NULL;
-	struct addrinfo *tmp;
-
-	ZeroMemory(&hint, sizeof(hint));
-	hint.ai_family = AF_INET;
-	hint.ai_socktype = SOCK_STREAM;
-	hint.ai_protocol = IPPROTO_TCP;
-
-	recv = getaddrinfo(hostname, port, &hint, &dest);
-	if (recv == SOCKET_ERROR) {
-		print_getaddrinfo_error(recv);
-		goto loop_end;
+	switch (error) {
+	case WAIT_ABANDONED_0:
+		printf("WAIT_ABANDONED\n");
+		break;
+	case WAIT_ABANDONED_0 + 1:
+		printf("WAIT_ABANDONED_0 + 1");
+	case WAIT_TIMEOUT:
+		printf("WAIT_TIMEOUT\n");
+		break;
+	case WAIT_FAILED:
+		printf("WAIT_FAILED: %d\n", GetLastError());
+		break;
 	}
-	tmp = dest;
-	do {
-		sock = socket(dest->ai_family, dest->ai_socktype, dest->ai_protocol);
-		if (sock == INVALID_SOCKET) {
-			printf("INVALID_SOCKET\n");
-			goto loop_end;
-		}
-		/* This fucking bastard function uses unicode by default, which
-		is twice as wide as a char, therefore to print the fucking string
-		properly, use %ls rather than %s cunt motherfucker bitch */
-		recv = WSAAddressToString((LPSOCKADDR)dest->ai_addr,
-			(DWORD)dest->ai_addrlen,
-			NULL,
-			ip,
-			&sizeofip);
-		if (recv != 0) {
-			print_wsaaddresstostring_error(recv);
-		}
-		printf("%ls\n", ip);
-
-		recv = connect(sock, dest->ai_addr, (int)dest->ai_addrlen);
-		if (recv == SOCKET_ERROR) {
-			printf("Filtered");
-			goto loop_end;
-		}
-		printf("Success\n");
-loop_end:
-		closesocket(sock);
-		tmp = dest->ai_next;
-		printf("dest->ai_next = %d", tmp);
-	} while (tmp != NULL);
 }
 
 void print_wsastartup_error(int error)
